63334fec2c9e4e0675bcb075ba2dde0a66f7ff5f
iverene <causapiniverenegrace@gmail.com>
2025-12-05 20:23:55 +0800

    add GET

--- server.js
+++ server.js
@@ -212,3 +212,7 @@ app.get("/api/health", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
